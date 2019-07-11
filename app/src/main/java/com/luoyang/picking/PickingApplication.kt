fed1d032b377afbb24c7bf67d2a67f852a8d91f4
LuoYang <toluoyang@qq.com>
2019-07-11 19:19:31 +0800

    添加token缓存

--- app/src/main/java/com/luoyang/picking/PickingApplication.kt
+++ app/src/main/java/com/luoyang/picking/PickingApplication.kt
@@ -3,14 +3,32 @@ package com.luoyang.picking
 import android.app.Activity
 import android.app.Application
 import android.os.Process
-import androidx.lifecycle.MutableLiveData
+import android.preference.PreferenceManager
+import com.google.gson.Gson
 import com.luoyang.picking.data.model.UserInfo
 import timber.log.Timber
 
 
 class PickingApplication : Application() {
     private val activitys = ArrayList<Activity>()
+    private val preference by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
     var userInfo: UserInfo? = null
+        set(value) {
+            field = value
+            preference.edit().putString("userInfo", Gson().toJson(value)).apply()
+        }
+        get() {
+            if (field == null) {
+                val string = preference.getString("userInfo", null)
+                if (string != null) {
+                    return Gson().fromJson(string, UserInfo::class.java)
+                } else {
+                    return null
+                }
+            } else {
+                return field
+            }
+        }
 
     companion object {
         lateinit var application: PickingApplication
